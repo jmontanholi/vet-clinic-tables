5f823b626ded33da1fcbf862a195458cd384f47d
jmontanholi <jonunes9326@hotmail.com.br>
2021-09-20 14:50:08 -0300

    Fix code reviewer requests

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
   id INT PRIMARY KEY, 
-  name CHAR(30), 
+  name VARCHAR(30), 
   date_of_birth DATE, 
   escape_attempts INT, 
   neutered BOOL, 
