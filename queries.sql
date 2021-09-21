1f6409e4747985025e02edb2d967847949c7fadb
jmontanholi <jonunes9326@hotmail.com.br>
2021-09-21 12:43:49 -0300

    Finish queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
-SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT escape_attempts, name FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true; 'OR' SELECT name FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon'; 'OR' SELECT name FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3; 'OR' SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '01-01-1990' AND '31-12-2000' GROUP BY species;
