5f6f7db995b0ddcd45e3c6c7d1dec672015a5536
jmontanholi <jonunes9326@hotmail.com.br>
2021-09-22 15:23:25 -0300

    Finish implementing requests from code reviewer

--- data.sql
+++ data.sql
@@ -2,42 +2,98 @@
 INSERT INTO animals 
               (name, date_of_birth, escape_attempts, neutered, weight_kg) 
             VALUES
-              ('Charmander', '08-02-2020', 0, false, -11),
-              ('Plantmon', '15-11-2022', 2, true, -5.7),
-              ('Squirtle', '2-04-1993', 3, false, -12.13),
-              ('Angemon', '12-06-2005', 1, true, -45),
-              ('Boarmon', '07-06-2005', 7, true, 20.4),
-              ('Blossom', '13-10-1998', 3, true, 17),
-              ('Agumon', '03-02-2020', 0, true, 10.23),
-              ('Gabumon', '15-11-2018', 2, true, 8),
-              ('Pikachu', '07-01-2021', 1, false, 15.04),
+              ('Charmander', '08-02-2020', 0, false, -11);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Plantmon', '15-11-2022', 2, true, -5.7);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES              
+              ('Squirtle', '2-04-1993', 3, false, -12.13);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Angemon', '12-06-2005', 1, true, -45);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Boarmon', '07-06-2005', 7, true, 20.4);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Blossom', '13-10-1998', 3, true, 17);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Agumon', '03-02-2020', 0, true, 10.23);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Gabumon', '15-11-2018', 2, true, 8);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
+              ('Pikachu', '07-01-2021', 1, false, 15.04);
+
+INSERT INTO animals 
+              (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+            VALUES
               ('Devimon', '12-04-2017', 5, true, 11);
 
 INSERT INTO owners
               (full_name, age)
             VALUES
-              ('Sam Smith', 34), 
-              ('Jennifer Orwell', 19), 
-              ('Bob', 45), 
-              ('Melody Pond', 77), 
-              ('Dean Winchester', 14),
+              ('Sam Smith', 34);
+
+INSERT INTO owners
+              (full_name, age)
+            VALUES
+              ('Jennifer Orwell', 19);
+
+INSERT INTO owners
+              (full_name, age)
+            VALUES
+              ('Bob', 45);
+
+INSERT INTO owners
+              (full_name, age)
+            VALUES
+              ('Melody Pond', 77);
+
+INSERT INTO owners
+              (full_name, age)
+            VALUES
+              ('Dean Winchester', 14);
+
+INSERT INTO owners
+              (full_name, age)
+            VALUES
               ('Jodie Whittaker', 38);
 
 INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
 
 BEGIN TRANSACTION;
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
 SELECT * FROM animals;
 COMMIT;
 SELECT * FROM animals;
 
 BEGIN TRANSACTION;
-UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
-UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
-UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
-UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name = 'Angemon' OR name = 'Boarmon';
 SELECT full_name AS owner, name FROM animals JOIN owners ON animals.owner_id = owners.id;
 COMMIT;
 SELECT full_name AS owner, name FROM animals JOIN owners ON animals.owner_id = owners.id;
